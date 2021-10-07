65b6aa4fbb29f9a2a8388ebbdc8a11b0d2e4188e
Alex <flowalex@vivaldi.net>
2021-10-06 20:01:01 -0500

    basic web scraper

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 use select::document::Document;
 use select::predicate::Name;
+use std::collections::HashSet;
 
 fn main() {
     let client = reqwest::blocking::Client::new();
@@ -11,8 +12,11 @@ fn main() {
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
 
-    Document::from(body.as_str())
+    let found_urls = Document::from(body.as_str())
         .find(Name("a"))
         .filter_map(|n| n.attr("href"))
-        .for_each(|x| println!("{}", x));
+        .map(str::to_string)
+        .collect::<HashSet<String>>();
+    println!("URLs: {:#?}", found_urls)
+
 }
